a2ef0b30b2978984ddc6594904d1c53bc5e0ad2d
margon23 <maliozturk2002@yahoo.com>
2019-06-16 19:03:03 +0300

    removed email infos

--- btre/settings.py
+++ btre/settings.py
@@ -144,11 +144,7 @@ MESSAGE_TAGS = {
 }
 
 # Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'uzemyazilim@gmail.com'
-EMAIL_HOST_PASSWORD = '23TRkhG8V7Q'
-EMAIL_USE_TLS = True
+
 
 try:
     from .local_settings import *
